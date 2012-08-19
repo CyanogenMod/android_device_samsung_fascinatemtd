66fb07798b868674bdaf5b7eff5745ae9a5e7340
jt1134 <jt1134@gmail.com>
2012-08-19 03:16:41 -0500

    fix bootanimation hacks
    Change-Id: I0de1ecbb67657415eb315122db62cb6d6e2a958e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,8 +106,8 @@ WIFI_DRIVER_FW_PATH_P2P     := "/vendor/firmware/fw_bcmdhd_p2p.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 
 # Bootanimation
-TARGET_BOOTANIMATION_PRELOAD := false
-TARGET_BOOTANIMATION_TEXTURE_CACHE := false
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 TARGET_BOOTANIMATION_USE_RGB565 := true
 
 # Vold
