5d3aed6fbe4a74b4b75e5c5f37fa4010a458d6dc
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-02-15 20:35:30 -0600

    fascinatemtd: use default audio policy
    No point in extending and compiling another audio policy when we're
    not modifying it in anyway.
    
    Change-Id: Ia77395543dcaa33f0af76cca2e7d81c2c58ff7ff

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -26,16 +26,4 @@ LOCAL_C_INCLUDES += \
 
 include $(BUILD_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := AudioPolicyManager.cpp
-LOCAL_SHARED_LIBRARIES := libcutils libutils
-LOCAL_STATIC_LIBRARIES := libmedia_helper
-LOCAL_WHOLE_STATIC_LIBRARIES := libaudiopolicy_legacy
-LOCAL_MODULE := audio_policy.aries
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
-
 endif
