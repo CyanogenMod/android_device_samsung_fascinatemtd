a609855f64032da96ac77d32bd8a0e5094ab4db5
Richard Hansen <cyanogenmod@rhansen.org>
2013-02-19 14:00:09 -0500

    fascinatemtd: clean up proprietary file extraction script
     * add proper quoting to variable expansions
     * send error messages to stderr
     * use printf to avoid portability issues with echo
     * don't silently fail when there is an error
     * don't assume the script is run from the directory containing the
       script
     * filter out whitespace-only lines in proprietary-files.txt
     * properly handle whitespace in filenames
     * log progress
    
    Change-Id: I9723cd8d35b0cada8c685aec52f7417b7e595d22

--- extract-files.sh
+++ extract-files.sh
@@ -3,31 +3,39 @@
 VENDOR=samsung
 DEVICE=fascinatemtd
 
+log() { printf %s\\n "$*"; }
+error() { log "ERROR: $@" >&2; }
+fatal() { error "$@"; exit 1; }
+try() { "$@" || fatal "'$*' failed"; }
+
+mydir=$(try dirname "$0") || exit 1
+try cd "${mydir}"
+
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
+try rm -rf "${BASE}"
 CMD="adb pull "
 
 if [ -e "$1" ]; then
-    rm -rf tmp
-    mkdir tmp
-    if ! unzip -q "$1" -d tmp; then
-        echo Failed to unzip "$1"
-        exit 1
-    fi
+    try rm -rf tmp
+    try mkdir tmp
+    unzip -q "$1" -d tmp || fatal "Failed to unzip $1"
     CMD="cp tmp"
 fi
 
-for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-    fi
-    if ! $CMD/system/$FILE $BASE/$FILE; then
-        echo Failed to pull or copy $FILE
-        exit 1
-    fi
-done
+log "Copying proprietary files from device..."
+# first make sure we can read proprietary-files.txt
+proprietary_files_txt=$(try cat proprietary-files.txt) || exit 1
+# filter out comments and blank/whitespace-only lines
+printf %s\\n "${proprietary_files_txt}" |
+grep -Ev '^[[:space:]]*(#|$)' |
+while IFS= read -r FILE; do
+    DIR=$(try dirname "${FILE}") || exit 1
+    try mkdir -p "${BASE}"/"${DIR}"
+    log "  /system/${FILE}"
+    ${CMD}/system/"${FILE}" "${BASE}"/"${FILE}" \
+        || fatal "Failed to pull or copy $FILE"
+done || exit 1
 
-rm -rf tmp
+try rm -rf tmp
 
-./setup-makefiles.sh
+./setup-makefiles.sh || exit 1
