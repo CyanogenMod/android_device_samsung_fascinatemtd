c90a6357784a47236ccd10201fd13d22ba512283
jt1134 <jt1134@gmail.com>
2013-02-21 19:30:24 -0600

    fascinatemtd: allow extract script to pull files from a zip
    Change-Id: Ib577817ee846ed2280a39f4520e14cccc50f0b1f

--- extract-files.sh
+++ extract-files.sh
@@ -5,13 +5,29 @@ DEVICE=fascinatemtd
 
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 rm -rf $BASE/*
+CMD="adb pull "
+
+if [ -e "$1" ]; then
+    rm -rf tmp
+    mkdir tmp
+    if ! unzip -q "$1" -d tmp; then
+        echo Failed to unzip "$1"
+        exit 1
+    fi
+    CMD="cp tmp"
+fi
 
 for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
     DIR=`dirname $FILE`
     if [ ! -d $BASE/$DIR ]; then
         mkdir -p $BASE/$DIR
     fi
-    adb pull /system/$FILE $BASE/$FILE
+    if ! $CMD/system/$FILE $BASE/$FILE; then
+        echo Failed to pull or copy $FILE
+        exit 1
+    fi
 done
+
+rm -rf tmp
 
 ./setup-makefiles.sh
